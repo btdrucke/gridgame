11db1b51f9007c374ca64bf077f1c0818b738c93
Benjamin Drucker <benjamin.t.drucker@intel.com>
2011-12-13 13:36:14 -0800

    Moving isntantiation of Game.Logic to the start of execution (was on DOM ready)

--- gridgame/js/gg-main.js
+++ gridgame/js/gg-main.js
@@ -1,10 +1,6 @@
+//var topo = new Game.Topology.Plane();
 //var topo = new Game.Topology.Plane({xSize:30, ySize:30, elemSize:20});
 //var topo = new Game.Topology.Cylinder();
 //var topo = new Game.Topology.Torus({xSize:50, ySize:15});
 var topo = new Game.Topology.Torus();
-
-window.addEventListener("DOMContentLoaded", function() {
-    window.setTimeout(function() {
-        var logic = new Game.Logic.Bomb({topology: topo});
-    }, 500);
-}, false);
+var logic = new Game.Logic.Bomb({topology: topo});
